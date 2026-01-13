313c1a7857504b17848eac806b42f0149072882f
Rayan <rayanatrouni@gmail.com>
2026-01-13 21:56:59 +0100

    Create pages Rooms and Bookings

---
+++ react-project/src/pages/Booking.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Booking() {
+  return (
+    <div>
+      <h2>Réservation</h2>
+      <p>Formulaire de réservation (à implémenter).</p>
+    </div>
+  )
+}
---
+++ react-project/src/pages/Rooms.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Rooms() {
+  return (
+    <div>
+      <h2>Espaces</h2>
+      <p>Espaces disponibles</p>
+    </div>
+  )
+}
